5c0863f478cfdb1338c83c207daf7b360bdddee2
SandeepKarukayil <sandeepkarukayil93@gmail.com>
2023-02-20 19:39:56 +0530

    task 5

--- basicdom/task4thpart.js
+++ basicdom/task4thpart.js
@@ -1,6 +1,6 @@
 var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
+// console.log(items);
+// console.log(items[1]);
 // items[1].textContent = HEllo2;
 // items[1].style.fontWeight = 'bold';
 // items[1].style.backgroundColor = 'yellow';
@@ -10,7 +10,31 @@ console.log(items[1]);
 // }
 
 //sharpener task 4
-items[2].style.backgroundColor = 'green';
+// items[2].style.backgroundColor = 'green';
+// for (let i = 0; i < items.length; i++) {
+//   items[i].style.fontWeight = 'bold';
+// }
+
+// // getElementsBYtagname
+
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = Hello2;
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
+
+// for (let i = 0; i < li.length; i++) {
+//   li[i].style.fontWeight = 'bold';
+// }
+
+// sharpener task 5
+// editing by classname
 for (let i = 0; i < items.length; i++) {
-  items[i].style.fontWeight = 'bold';
+  items[i].style.backgroundColor = 'yellow';
+}
+// by changing background color by tag
+var li = document.getElementsByTagName('li');
+for (let i = 0; i < li.length; i++) {
+  li[i].style.backgroundColor = 'pink';
 }
