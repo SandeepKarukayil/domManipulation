ff5f4bb8c2608e1085da0a2d88bef74cd06e1194
SandeepKarukayil <sandeepkarukayil93@gmail.com>
2023-02-21 10:59:00 +0530

    6th task

--- basicdom/task4thpart.js
+++ basicdom/task4thpart.js
@@ -30,11 +30,60 @@ var items = document.getElementsByClassName('list-group-item');
 
 // sharpener task 5
 // editing by classname
-for (let i = 0; i < items.length; i++) {
-  items[i].style.backgroundColor = 'yellow';
-}
-// by changing background color by tag
-var li = document.getElementsByTagName('li');
-for (let i = 0; i < li.length; i++) {
-  li[i].style.backgroundColor = 'pink';
+// for (let i = 0; i < items.length; i++) {
+//   items[i].style.backgroundColor = 'yellow';
+// }
+// // by changing background color by tag
+// var li = document.getElementsByTagName('li');
+// for (let i = 0; i < li.length; i++) {
+//   li[i].style.backgroundColor = 'pink';
+// }
+
+// // Query selector
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc'
+
+// var input = document.querySelector('input');
+// input.value = 'Hello World'
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value = "SEND"
+// var item = Document.querySelector('.list-group-item');
+// item.style.color = 'red';
+
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color = 'coral'
+//Query Selector All
+
+// var titles = document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0].textContent = 'Hello';
+
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+
+// for (let i = 0; i < odd.length; i++) {
+//   odd[i].style.backgroundColor = '#f4f4f4';
+
+// }
+
+
+//Sharpener TASK  6
+
+// 1.make 2nd item have green background color
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor = 'green';
+
+// // 2. make 3rd item invisible
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.visibility = 'hidden';
+
+var secondItem = document.querySelectorAll('.list-group-item');
+secondItem[1].style.color = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+for (let i = 0; i < odd.length; i++) {
+  odd[i].style.backgroundColor = 'green';
 }
