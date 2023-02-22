e25a7fc21093da56ef98521fae88723c299e3c8b
SandeepKarukayil <sandeepkarukayil93@gmail.com>
2023-02-22 20:02:44 +0530

    task 11 object stored

--- basicdom/booking application/main.js
+++ basicdom/booking application/main.js
@@ -8,10 +8,22 @@ function addItem(e) {
   e.preventDefault();
 
   //get input value
-  var newItem = document.getElementById('name').value;
-  var newItem1 = document.getElementById('email').value;
+  var newName = document.getElementById('name').value;
+  var newEmail = document.getElementById('email').value;
 
   //put in local storage
-  localStorage.setItem("name", newItem);
-  localStorage.setItem("email", newItem1);
+  // localStorage.setItem("name", newName);
+  // localStorage.setItem("email", newEmail);
+
+  let infoObj = {
+    name: newName,
+    email: newEmail
+  };
+
+  let infoObj_serialized = JSON.stringify(infoObj);
+
+  localStorage.setItem("key", infoObj_serialized);
+
+  let infoObj_deserialized = JSON.parse(localStorage.getItem("infoObj"))
+
 }
