407ae54bba5ef74ffe0db785bf5a600458c5cd70
SandeepKarukayil <sandeepkarukayil93@gmail.com>
2023-02-23 10:47:34 +0530

    task 12

--- basicdom/booking application/main.js
+++ basicdom/booking application/main.js
@@ -15,6 +15,22 @@ function addItem(e) {
   // localStorage.setItem("name", newName);
   // localStorage.setItem("email", newEmail);
 
+
+  //create new li element
+
+  var data = document.createElement('li');
+
+  // Add class
+
+  data.className = 'list-group-item'
+
+  //add text node with input value
+
+  data.appendChild(document.createTextNode(newName));
+  data.appendChild(document.createTextNode('  ' + newEmail));
+  //append child
+  form.appendChild(data);
+
   let infoObj = {
     name: newName,
     email: newEmail
@@ -22,7 +38,7 @@ function addItem(e) {
 
   let infoObj_serialized = JSON.stringify(infoObj);
 
-  localStorage.setItem("key", infoObj_serialized);
+  localStorage.setItem(infoObj.email, infoObj_serialized);
 
   let infoObj_deserialized = JSON.parse(localStorage.getItem("infoObj"))
 
